a9067d0cf30c856bd7b11396214864b1e1bfab4c
Wang YiMing <wangyimig@icloud.com>
2020-04-22 18:16:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PythonTestStudy
+ python单元测试和集成测试学习
---
+++ tests/unit/test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from my_sum import sum
+from fractions import Fraction
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+
+        :return:
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 4)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+    def test_bad_type(self):
+        data = 'banana'
+        with self.assertRaises(TypeError):
+            result = sum(data)
+
+
+if __name__ == '__main__':
+    unittest.main()
